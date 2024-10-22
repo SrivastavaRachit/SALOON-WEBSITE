6b867cef5cc00e1a336127849c9ae36aa36a491a
abid-zaidi <zaidiabidmohsin@gmail.com>
2024-10-23 03:27:49 +0530

    v

--- frontend/src/components/OurStory3/OurStory3.jsx
+++ frontend/src/components/OurStory3/OurStory3.jsx
@@ -4,13 +4,6 @@ import productImage from '../../assets/b5.png'; // Replace with your image path
 const OurProducts = () => {
     return (
         <section className="bg-blush min-h-screen flex flex-col md:flex-row items-center">
-            <div className="w-full md:w-1/2 mb-0 flex justify-center items-center">
-                <img 
-                    src={productImage} 
-                    alt="Our Products" 
-                    className="max-w-full h-auto" 
-                />
-            </div>
             <div className="w-full md:w-1/2 py-16 px-8">
                <h1 className="text-4xl font-bold text-black mb-6 text-center" style={{ fontFamily: 'Zeyada' }}>Our Products</h1>
 <p className="text-gray-700 mb-6 text-sm sm:text-base lg:text-lg leading-relaxed">
@@ -19,6 +12,13 @@ const OurProducts = () => {
     in Cosmetics (CCIC), which is an NGO founded by 8 international animal protection groups such as: American Anti-Vivisection Society, 
     Animal Alliance of Canada, Doris Day Animal League, etc.
 </p>
+            </div>
+            <div className="w-full md:w-1/2 mb-0 flex justify-center items-center">
+                <img 
+                    src={productImage} 
+                    alt="Our Products" 
+                    className="max-w-full h-auto" 
+                />
             </div>
         </section>
     );
